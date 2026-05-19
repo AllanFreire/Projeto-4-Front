d4b6e2a6c23d7f60a71a558847c44c2c33a4d1fb
Allan <allan.ragazzofreire@gmail.com>
2026-05-19 09:17:14 -0300

    script: js menu hamburguer

--- src/js/scipt.js
+++ src/js/scipt.js
@@ -15,3 +15,18 @@ window.onload = function(){
 function trocar(cor){
     document.body.style.background=cor;
 }
+
+//Menu Hamburguer
+
+//  MENU HAMBURGUER
+
+document.addEventListener("DOMContentLoaded",()=>{
+    const menuIcone = document.getElementById("menu-icone");
+    const navMenu =document.querySelector("nav");
+    if(menuIcone && navMenu){
+        menuIcone.onclick=()=>{
+                navMenu.classList.toggle("active");
+                menuIcone.classList.toggle("open");
+        }
+    }
+})
